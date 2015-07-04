ecd7e430fd596f68dc1974087751f94ea17a1fcd
NAKAMURA Yasuhiro <s1513088@u.tsukuba.ac.jp>
2015-07-04 11:42:59 +0900

    spec6: 指定したIDを持つレシピだけを出力する

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require 'JSON'
 
+def show_recipe(recipe, id)
+	puts "#{id}: #{recipe}"
+end
+
+def show_recipes_all(recipes)
+	recipes.each_with_index do |recipe, id|
+		show_recipe(recipe, id)
+	end
+end
+
+def show_recipe_by_id(recipes, id)
+	show_recipe(recipes[id], id)
+end
+
 recipe_file_path = ARGV[0]
 json = File.open(recipe_file_path).read
 recipes = JSON.parse(json)
 
-recipes.each_with_index do |recipe, id|
-	puts "#{id}: #{recipe}"
+show_id = ARGV[1].to_i
+if show_id
+	show_recipe_by_id(recipes, show_id)
+else
+	show_recipes_all(recipes)
 end
